0f2724a2d9eca5e9198b9ff91909124613de1701
tunguyendev0108 <144198869+tunguyendev0108@users.noreply.github.com>
2024-03-14 15:23:15 +0700

    Add spring-boot-customer-management

---
+++ 17. Spring Boot/spring-boot-customer-management/src/main/java/com/codegym/controller/CustomerController.java
@@ -0,0 +1,84 @@
+package com.codegym.controller;
+
+import com.codegym.model.Customer;
+import com.codegym.service.CustomerService;
+import com.codegym.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm(){
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer",new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer",new Customer());
+        modelAndView.addObject("message","New customer created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listCustomers(){
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        modelAndView.addObject("customer",customerService.findAll());
+        return modelAndView;
+    }
+
+    @GetMapping("/update/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id){
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/customer/update");
+            modelAndView.addObject("customer",customer.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/error_404");
+        }
+    }
+
+    @PostMapping("/update")
+    public ModelAndView updateCustomer(@ModelAttribute("customer")Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/update");
+        modelAndView.addObject("customer",customer);
+        modelAndView.addObject("message","Customer update successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("/customer/delete");
+            modelAndView.addObject("customer",customer.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/error_404");
+        }
+    }
+
+    @PostMapping("/delete")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.remove(customer.getId());
+        return "redirect:/customers";
+    }
+}
---
+++ 17. Spring Boot/spring-boot-customer-management/src/main/java/com/codegym/model/Customer.java
@@ -0,0 +1,19 @@
+package com.codegym.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
