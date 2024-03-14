56d9dd7741cf50a9f5ac18e153ddacb5d1cbd5a8
tunguyendev0108 <144198869+tunguyendev0108@users.noreply.github.com>
2024-03-14 14:18:28 +0700

    Add first-spring-boot

---
+++ 17. Spring Boot/codegym/src/main/java/com/codegym/model/Customer.java
@@ -0,0 +1,48 @@
+package com.codegym.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+
+    public Customer(){}
+    public Customer(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ 17. Spring Boot/first-spring-boot/src/main/java/com/codegym/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public ModelAndView showGreeting(){
+        return new ModelAndView("/greeting");
+    }
+
+}
